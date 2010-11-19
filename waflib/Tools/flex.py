b7cb71bf43573eddb58a36d6820a81cb16878ea6
tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85 <none@none>
2010-11-19 20:58:24 +0000

    enough docs for today
    --HG--
    extra : convert_revision : svn%3Af0382ac9-c320-0410-b3f0-b508d59f5a85/trunk%4010449

---
+++ waflib/Tools/flex.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# John O'Meara, 2006
+# Thomas Nagy, 2006-2010 (ita)
+
+"""
+The **flex** program is a code generator which creates C or C++ files.
+The generated files are compiled into object files.
+"""
+
+import waflib.TaskGen
+
+def decide_ext(self, node):
+	if 'cxx' in self.features:
+		return ['.lex.cc']
+	return ['.lex.c']
+
+waflib.TaskGen.declare_chain(
+	name = 'flex',
+	rule = '${FLEX} -o${TGT} ${FLEXFLAGS} ${SRC}',
+	ext_in = '.l',
+	decider = decide_ext
+)
+
+def configure(conf):
+	"""
+	Detect the *flex* program
+	"""
+	conf.find_program('flex', var='FLEX')
+
